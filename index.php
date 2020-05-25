87018a823beb550976cccc7aa93bf3859510b69b
Hyun Sol Lee <pjf1548@autuni.ac.nz>
2020-05-25 13:54:27 +1200

    testing

--- index.php
+++ index.php
@@ -1,2 +1,3 @@
 <a href="login.php">Log in</a> <br />
 <a href="signup.php">Sign up</a> <br />
+hello
--- signup.php
+++ signup.php
@@ -5,7 +5,7 @@
 
 <body>
 <h1>Sign up Page</h1>
-	<form method="post" action="member_add.php">
+	<form method="post">
 		<p>	<label for="username">Username: </label>
 			<input type="text" name="username" id="username" /></p>
 		<p>	<label for="pwd">Password: </label>
@@ -14,16 +14,30 @@
 			<input type="password" name="pwd2" id="pwd2" /></p>
 		<p>	<label for="email">Enter email: </label>
 			<input type="text" name="email" id="email" /></p>
-		<p>	<label for="occupation">User Type: </label>
+		<p>	<label for="usertype">User Type: </label>
 		<select name="usertype">
 			<option value="student">Student</option>
 			<option value="researcher">Researcher</option>
 			<option value="affiliation ">Affiliation</option>
 			<option value="host">SERL Host</option>
 			<option value="analyst">SERL Analyst</option> </select></p>
-        <p>	<input type="submit" value="Sign up" />
+        <p>	<input type="submit" name="submit" value="Sign up" />
 		<input type="reset" value="Reset" name="reset" /> <br /><br />
 		<a href="index.php">Return to Home Page </a></p>
 	</form>
 </body>
 </html>
+
+<?php
+if(isset($_POST['submit'])){ //if button is clicked
+// Get data from the form
+$username = $_POST["username"];
+$pwd = $_POST["pwd"];
+$pwd2 = $_POST["pwd2"];
+$email = $_POST["email"];
+$usertype = $_POST["usertype"];
+
+echo $username;
+echo "test";
+}
+?>
