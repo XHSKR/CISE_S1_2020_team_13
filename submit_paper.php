18ff7737b300bd3bb8fd8112d2adb1e4d47a48ea
Hyun Sol Lee <pjf1548@autuni.ac.nz>
2020-06-13 11:57:16 +1200

    submit paper implemented

--- submit_paper.php
+++ submit_paper.php
@@ -9,9 +9,11 @@
 
 <?php
 include 'login_check.php';
+$username = $_SESSION['username'];
 ?>
 
 <h3>Please fill in every forms to submit a paper.</h3></label>
+<form method="post">
 <p> <label>Author: </label></p>
 <p><input type="text" name="author" id="author" placeholder = "Aniche"/></p>
 <p> <label>Title: </label></p>
@@ -21,7 +23,64 @@ include 'login_check.php';
 <p><label>Description:</label></p>
 <p><textarea id="description" name="description" rows="4" cols="50"></textarea></p>
 <p> <input type="submit" name="submit" value="Submit" /></p>
+</form>
+<?php
+if (isset($_POST['submit'])) { //if button is clicked
+    // Get data from the form
+    $author = $_POST["author"];
+    $title = $_POST["title"];
+    $year = $_POST["year"];
+    $description = $_POST["description"];
 
+    //check if all required data is retrieved
+    if (empty($author) || empty($title) || empty($year) || empty($description)) {
+        echo "
+            <script>
+            alert('Please fill in every required forms.');
+            history.back();
+            </script>
+            ";
+        return;
+    }
+
+    require_once "settings.php";
+    // Checks if connection is successful
+    if (!$conn) {
+        // Displays an error message
+        echo "<b>Database connection failure</b>";
+    } else {
+        // Upon successful connection
+        $query = "SELECT * FROM paper";
+        $result = mysqli_query($conn, $query);
+        //check if table exists
+        if (!$result) { //if table does not exist
+            echo "<b>Table does not exist.</b>";
+        } else {
+            //get today date
+            $date = date('yy-m-d');
+            // Set up the SQL command to add the data into the table
+            $query = "insert into paper"
+                . "(submitter, author, title, year, description, submitted_date, status)"
+                . "values"
+                . "('$username','$author','$title','$year', '$description', '$date', 'pending')";
+            // executes the query
+            $result = mysqli_query($conn, $query);
+            // checks if the execution was successful
+            if (!$result) {
+                echo "<p>Something is wrong with ", $query, "</p>";
+            } else {
+                // display an operation successful message
+                echo "
+                <script>
+                alert('Successfully submitted !');
+                </script>
+                ";
+                echo "<meta http-equiv='refresh' content='0;url=/'>";
+            }
+        }
+    }
+}
+?>
 <p><a href='/'>Back to Main Page</a></p>
 </body>
 </html>
